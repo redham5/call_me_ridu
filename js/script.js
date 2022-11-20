b346208696743a9ca146cdfbafdd61150ec5c145
Ridu <fi.redham16@gmail.com>
2022-11-20 18:17:16 +0600

    20-nov-22 update

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('.slick').slick({
+      
+        autoplay: true,
+        autoplaySpeed: 1500,
+        arrows: false,
+        fade: true,
+        focusOnSelect: true,
+        dots: true,
+
+    });
+  });
